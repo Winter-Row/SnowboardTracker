eb1f9d9b01f48ed6e86cd93ba18d1432a1c5fd30
Winter-Row <winter.e.r@outlook.com>
2023-03-26 12:31:31 -0400

    have began building out login and register

--- routes/trips.js
+++ routes/trips.js
@@ -42,5 +42,51 @@ router.post('/add', (req,res,next) =>{
     });
 });
 
+//GET handler for /trips/delete
+router.get('/delete/:_id',(req,res,next)=>{
+    Trip.remove({
+        _id: req.params._id
+    },(err)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect('/trips');
+        }
+    });
+});
+
+//GET handler for /trips/edit
+router.get('/edit/:_id',(req,res,next)=>{
+    Trip.findById(req.params._id, (err, trip)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.render('trips/edit', {title: 'Edit a trip', trip: trip});
+        }
+    });
+});
+
+//POST handler for /trips/edit
+router.post('/edit/:_id',(req,res,next)=>{
+    Trip.findOneAndUpdate(
+        {
+            _id: req.params._id
+        },
+        {
+            resortName: req.body.resortName,
+            date: req.body.date,
+            timeSpent: req.body.timeSpent,
+            spendings: req.body.spendings
+        },
+        (err, updatedTrip)=>{
+            if(err){
+                console.log(err);
+            }else{
+                res.redirect('/trips');
+            }
+        }
+    )
+});
+
 
 module.exports = router;
