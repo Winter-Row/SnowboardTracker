628ebfff33c711561948c3002574b54b41c6dfcc
Winter-Row <winter.e.r@outlook.com>
2023-03-28 12:30:57 -0400

    add a check to see if user is logged in

--- routes/trips.js
+++ routes/trips.js
@@ -5,6 +5,16 @@ var router = express.Router();
 //import models
 const Trip = require("../models/trips");
 
+//method to check if the user is logged in
+function IsLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next(); //moves to the next middleware in the pipleline
+  }
+  else{
+    res.redirect('/login'); //send a public user back to login
+  }
+}
+
 //GET /trips
 router.get('/', function(req, res, next) {
 //   res.render('trips/index', { title: 'Trips' });
@@ -22,12 +32,12 @@ router.get('/', function(req, res, next) {
 });
 
 //GET /trips/add
-router.get('/add', function(req, res, next) {
+router.get('/add', IsLoggedIn, function(req, res, next) {
     res.render('trips/add', { title: 'Add a recent trip', user: req.user });
 });
 
 //POST /trips/add
-router.post('/add', (req,res,next) =>{
+router.post('/add', IsLoggedIn, (req,res,next) =>{
     Trip.create({
         resortName: req.body.resortName,
         date: req.body.date,
@@ -44,7 +54,7 @@ router.post('/add', (req,res,next) =>{
 });
 
 //GET handler for /trips/delete
-router.get('/delete/:_id',(req,res,next)=>{
+router.get('/delete/:_id',IsLoggedIn, (req,res,next)=>{
     Trip.remove({
         _id: req.params._id
     },(err)=>{
@@ -57,7 +67,7 @@ router.get('/delete/:_id',(req,res,next)=>{
 });
 
 //GET handler for /trips/edit
-router.get('/edit/:_id',(req,res,next)=>{
+router.get('/edit/:_id',IsLoggedIn, (req,res,next)=>{
     Trip.findById(req.params._id, (err, trip)=>{
         if(err){
             console.log(err);
@@ -68,7 +78,7 @@ router.get('/edit/:_id',(req,res,next)=>{
 });
 
 //POST handler for /trips/edit
-router.post('/edit/:_id',(req,res,next)=>{
+router.post('/edit/:_id',IsLoggedIn, (req,res,next)=>{
     Trip.findOneAndUpdate(
         {
             _id: req.params._id
